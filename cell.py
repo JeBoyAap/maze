213f4a61057d85d4aa99b5c6469a8cc362a09140
Stijn Luiken <stijnphilipluiken@gmail.com>
2025-09-21 22:03:01 +0200

    V1

---
+++ cell.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Cell:
+    def __init__(self, cell_type, width, height, x_cor, y_cor):
+        self.cell_type = cell_type
+        self.width = width
+        self.height = height
+        self.x_cor = x_cor
+        self.y_cor = y_cor
+
+    def draw(self, screen):
+        print(self.x_cor, self.y_cor)
+        if self.cell_type == 0:
+            pygame.draw.rect(screen, "orange", (self.x_cor, self.y_cor, self.width, self.height))
+        elif self.cell_type == 1:
+            pygame.draw.rect(screen, "black", (self.x_cor, self.y_cor, self.width, self.height))
+
+def around(x, base):
+    return base * round(x/base)
