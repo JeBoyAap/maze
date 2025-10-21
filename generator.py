c02665d67aa15e9142f0b20f912765135207ea3c
JeBoyAap <151835368+JeBoyAap@users.noreply.github.com>
2025-10-21 22:00:06 +0200

    Added some effects to follow current_pos, optimised traceback logic

--- generator.py
+++ generator.py
@@ -12,40 +12,39 @@ def init_maze(grid_size_x, grid_size_y, use_seed, seed):
     if use_seed:
         random.seed(seed)
 
-def maze_generator(traceback, current_pos, cells, screen, grid_size_x, grid_size_y):
+def maze_generator(current_pos, prev_pos, cells, screen, grid_size_x, grid_size_y):
     current_x, current_y = current_pos
     visited[current_y][current_x] = True
     neighbours = find_neighbours(current_pos, grid_size_x, grid_size_y)
+
+    if current_pos in cells:
+        cells[current_pos].color_current_pos(screen)
+    if prev_pos in cells:
+        cells[prev_pos].make_path(screen)
+    prev_pos = current_pos
     if len(neighbours) > 0:
-        traceback = 0
         goal_pos = random.choice(neighbours)
         wall_pos = ((goal_pos[0] + current_pos[0]) // 2, (goal_pos[1] + current_pos[1]) // 2)
 
-        if (current_x, current_y) in cells:
-            cells[(current_x, current_y)].make_path(screen)
-        if wall_pos in cells:
-            cells[wall_pos].make_path(screen)
         if goal_pos in cells:
             cells[goal_pos].make_path(screen)
+        if wall_pos in cells:
+            cells[wall_pos].make_path(screen)
 
         traceback_stack.append(current_pos)
         maze[current_y][current_x] = 0
         current_pos = goal_pos
 
-    #temp solution
+
     else:
-        traceback += 1
-        try:
-            traceback %= len(traceback_stack)
-        except:
+        if not traceback_stack:
+            if prev_pos in cells:
+                cells[prev_pos].make_path(screen)
             return None, None
         else:
-            if debug: print(f"tb {traceback} % {len(traceback_stack)} = {traceback % len(traceback_stack)}")
-            traceback %= len(traceback_stack)
-            current_pos = traceback_stack[-traceback]
-            traceback_stack.remove(current_pos)
+            current_pos = traceback_stack.pop()
 
-    return current_pos, traceback
+    return current_pos, prev_pos
 
 def find_neighbours(current_pos, grid_size_x, grid_size_y):
     current_x, current_y = current_pos
